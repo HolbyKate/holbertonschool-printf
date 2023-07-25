db37ef85ee3b1f61f6e38b66bd267817c342e308
root <you@example.com>
2023-07-25 07:32:15 -0700

    fix: change type on the function to int

--- functions.c
+++ functions.c
@@ -2,9 +2,9 @@
 
 /**
  * _putchar - display one charactere
- * @c: the charactere ro display
+ * @c: the charactere to display
  *
- * Return: c
+ * Return: the character
  */
 int _putchar(char c)
 {
@@ -15,41 +15,48 @@ int _putchar(char c)
  * print_char - function that prints the char from the list
  * @list: list to choose the character
  *
+ * Return: number of characters
  */
 
-void print_char(va_list list)
+int print_char(va_list list)
 {
 	char c = va_arg(list, int);
 
-		_putchar(c);
+	return (_putchar (c));
 }
+
 
 /**
  * print_string - function that prints a string
- *@list: list the number of characters in the string
+ * @list: list the number of characters in the string
  *
- *Return : nothing
+ * Return: number of characters
  */
-void print_string(va_list list)
+int print_string(va_list list)
 {
 	int i = 0;
+	int count = 0;
 	char *str = va_arg(list, char*);
 
 	if (str == NULL)
-		return;
+		return (-1);
 
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
+		count++;
 	}
+	return (count);
 }
 
 /**
  * print_percent - function that handle the case %%
  * @list: list to recognize the case
+ * Return: number of characters
  */
-void print_percent(va_list __attribute__((unused)) list)
+int print_percent(va_list __attribute__((unused)) list)
 {
 	_putchar('%');
+	return (1);
 }
