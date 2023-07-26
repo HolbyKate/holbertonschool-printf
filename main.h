815255c1c3e7189a667968b78b998f48b67966f8
root <you@example.com>
2023-07-26 12:28:04 -0700

    fix: remove one prototype to reduce number of prototype

--- main.h
+++ main.h
@@ -23,6 +23,7 @@ int print_char(va_list list);
 int print_string(va_list list);
 int print_percent(va_list list);
 int print_format(char type, va_list list);
+int print_number(va_list list);
 /* main function of printf*/
 int _printf(const char *format, ...);
 #endif
