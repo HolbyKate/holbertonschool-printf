e4a26ccc3abf30eca9d5f9a237ebbfea85f91686
SpStigma <GitHubStigma@gmail.com>
2023-07-27 02:05:04 -0700

    add: New description betty approuved in the fct

--- my_printf.c
+++ my_printf.c
@@ -15,20 +15,26 @@ int _printf(const char *format, ...)
 	va_list list;
 
 	va_start(list, format);
-
+	/* check some condition format null and format only have % */
 	if (format ==  NULL || (format[0] == '%' && format[1] == '\0'))
 	{
 		return (-1);
 	}
+	/* Loop to iterating through the string */
 	for (i = 0; format[i] != '\0'; i++)
 	{
+		/* check if format == % */
 		if (format[i] == '%')
 		{
+			/* if found % check format[i] + 1 and attribute */
+			/* the format specifier if is found; else print % */
+			/* and the type */
 			count += print_format(format[i + 1], list);
 			i++;
 		}
 		else
 		{
+			/* print putchar[i] if % not find and count the char printed */
 			count++;
 			_putchar(format[i]);
 		}
